d3fb1b8a02e8d7b38dcf312305b923e90569523b
Sewer <seweratwork@gmail.com>
2019-07-22 21:15:27 +0100

    Add icon and release build cleanup script.

---
+++ Source/CleanForDeployment.cmd
@@ -0,0 +1,6 @@
+del /s /q "Output\Launcher\*.pdb"
+del /s /q "Output\Launcher\*.xml"
+del /s /q "Output\Launcher\*.exp"
+del /s /q "Output\Launcher\*.lib"
+del /s /q "Output\Launcher\*.iobj"
+del /s /q "Output\Launcher\*.ipdb"
