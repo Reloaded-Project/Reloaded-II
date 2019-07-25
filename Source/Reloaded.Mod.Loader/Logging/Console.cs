971a5d118676f23db9934c2d57b8413c87b6a767
Sewer <seweratwork@gmail.com>
2019-07-25 05:36:07 +0100

    Implement Console Banner

--- Source/Reloaded.Mod.Loader/Logging/Console.cs
+++ Source/Reloaded.Mod.Loader/Logging/Console.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using Colorful;
 using Reloaded.Mod.Interfaces;
 using Reloaded.Mod.Loader.Logging.Init;
 
@@ -15,6 +16,11 @@ namespace Reloaded.Mod.Loader.Logging
         public void ShowConsole()
         {
             ConsoleAllocator.Alloc();
+
+            Colorful.Console.BackgroundColor = BackgroundColor;
+            Colorful.Console.ForegroundColor = TextColor;
+            System.Console.Clear();
+            PrintBanner();
         }
 
         public void PrintMessage(string message, Color color)
@@ -43,5 +49,68 @@ namespace Reloaded.Mod.Loader.Logging
 
         public Color ColorLightBlue     { get; set; } = Color.FromArgb(154, 237, 254);
         public Color ColorLightBlueLight { get; set; } = Color.FromArgb(147, 224, 227);
+
+        /* Default Banner */
+        private void PrintBanner()
+        {
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+
+            Formatter[] formatter = {
+                new Formatter(@"    hMMM+ +MMMh", ColorRed),
+                new Formatter(@"   `MMMM` dMMM/", ColorRed),
+                new Formatter(@"   +MMMh .MMMN`", ColorRed),
+                new Formatter(@"   dMMM/ sMMMy ", ColorRed),
+                new Formatter(@"  -MMMN  NMMM: ", ColorRed),
+                new Formatter(@"  sMMMy :MMMm  ", ColorRed),
+                new Formatter(@"  NMMM- yMMMo  ", ColorRed),
+                new Formatter(@" :MMMm `MMMM.  ", ColorRed),
+                new Formatter(@" yMMMo /MMMd   ", ColorRed),
+                new Formatter(@"`MMMM. dMMM+   ", ColorRed),
+
+            };
+
+            string[] template = new[]
+            {
+                @"MMMMMMMMMMMMMMMMMMdo`    {0}",
+                @"MMMMMMMMMMMMMMMMMMMMh    {0}",
+                @"MMMM-          `yMMMN    {0}",
+                @"MMMM-          `sMMMN    {0}",
+                @"MMMM- .sMMMMMMMMMMMMd    {0}",
+                @"MMMM-   `sNMMMMMMMdo`    {0}",
+                @"MMMM-     `oNMMMMy-      {0}",
+                @"MMMM-       `oNMMMMh-    {0}",
+                @"MMMM-         `+mMMMMh:  {0}",
+                @"MMMM-            /mMMMMd:{0}"
+            };
+
+
+            // Hardcoded for this banner, please do not reuse.
+            // -3 from the templated {0}
+            WriteLinesCentered(template, formatter, -3);
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+        }
+
+        private void WriteLinesCentered(string[] lines, Formatter[] formatters, int characterOffset)
+        {
+            // Hardcoded for this banner, please do not reuse.
+            for (int x = 0; x < lines.Length; x++)
+            {
+                var line        = lines[x];
+                var formatter   = formatters[x];
+
+                // Get center, accounting for overflow.
+                int consolePointer = (System.Console.WindowWidth - line.Length + characterOffset) / 2;
+                if (consolePointer < 0)
+                    consolePointer = 0;
+
+                System.Console.SetCursorPosition(consolePointer, System.Console.CursorTop);
+                Colorful.Console.WriteLineFormatted(line, formatter, TextColor);
+            }
+
+            System.Console.SetCursorPosition(0, System.Console.CursorTop);
+        }
     }
 }
