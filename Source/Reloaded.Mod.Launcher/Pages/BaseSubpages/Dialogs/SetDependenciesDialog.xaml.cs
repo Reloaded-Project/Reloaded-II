24aebbc74938bc195ae96d53871fde0dbd9df5cc
Sewer <seweratwork@gmail.com>
2019-12-18 13:19:58 +0000

    Small reorganization in SetDependenciesDialog

--- Source/Reloaded.Mod.Launcher/Pages/BaseSubpages/Dialogs/SetDependenciesDialog.xaml.cs
+++ Source/Reloaded.Mod.Launcher/Pages/BaseSubpages/Dialogs/SetDependenciesDialog.xaml.cs
@@ -6,6 +6,7 @@ using Reloaded.Mod.Interfaces;
 using Reloaded.Mod.Launcher.Models.Model;
 using Reloaded.Mod.Launcher.Models.ViewModel;
 using Reloaded.Mod.Loader.IO.Structs;
+using Reloaded.Mod.Loader.IO.Weaving;
 using Reloaded.WPF.Theme.Default;
 using Reloaded.WPF.Utilities;
 
@@ -16,37 +17,56 @@ namespace Reloaded.Mod.Launcher.Pages.BaseSubpages.Dialogs
     /// </summary>
     public partial class SetDependenciesDialog : ReloadedWindow
     {
-        public ObservableCollection<BooleanGenericTuple<IModConfig>> Dependencies { get; set; } = new ObservableCollection<BooleanGenericTuple<IModConfig>>();
-        public ManageModsViewModel ManageModsViewModel { get; }
-        public ImageModPathTuple CurrentMod { get; }
-
-        private readonly CollectionViewSource _dependenciesViewSource;
+        public SetDependenciesDialogViewmodel ViewModel { get; set; }
 
         public SetDependenciesDialog(ManageModsViewModel manageModsViewModel)
         {
             InitializeComponent();
-            ManageModsViewModel = manageModsViewModel;
-            CurrentMod = manageModsViewModel.SelectedModTuple;
-
-            PopulateDependencies();
+            ViewModel = new SetDependenciesDialogViewmodel(manageModsViewModel, new ResourceManipulator(this.Contents));
             this.Closed += OnClosed;
+        }
 
-            // Setup filters
-            var manipulator = new ResourceManipulator(this.Contents);
+        private void OnClosed(object sender, EventArgs e) => ViewModel.SaveCurrentMod();
+        private void ModsFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) => ViewModel.RefreshModList();
+    }
+
+    public class SetDependenciesDialogViewmodel : ObservableObject
+    {
+        public ObservableCollection<BooleanGenericTuple<IModConfig>> Dependencies { get; set; } = new ObservableCollection<BooleanGenericTuple<IModConfig>>();
+        public ManageModsViewModel ManageModsViewModel { get; }
+        public ImageModPathTuple CurrentMod { get; }
+
+        public string ModsFilter { get; set; } = "";
+        private readonly CollectionViewSource _dependenciesViewSource;
+
+        public SetDependenciesDialogViewmodel(ManageModsViewModel manageModsViewModel, ResourceManipulator manipulator)
+        {
+            ManageModsViewModel = manageModsViewModel;
+            CurrentMod = ManageModsViewModel.SelectedModTuple;
+
             _dependenciesViewSource = manipulator.Get<CollectionViewSource>("SortedDependencies");
             _dependenciesViewSource.Filter += DependenciesViewSourceOnFilter;
+            PopulateDependencies();
+        }
+
+        public void RefreshModList() => _dependenciesViewSource.View.Refresh();
+
+        public void SaveCurrentMod()
+        {
+            CurrentMod.ModConfig.ModDependencies = Dependencies.Where(x => x.Enabled).Select(x => x.Generic.ModId).ToArray();
+            CurrentMod.Save();
         }
 
         private void DependenciesViewSourceOnFilter(object sender, FilterEventArgs e)
         {
-            if (this.ModsFilter.Text.Length <= 0)
+            if (this.ModsFilter.Length <= 0)
             {
                 e.Accepted = true;
                 return;
             }
 
             var tuple = (BooleanGenericTuple<IModConfig>)e.Item;
-            e.Accepted = tuple.Generic.ModName.IndexOf(this.ModsFilter.Text, StringComparison.InvariantCultureIgnoreCase) >= 0;
+            e.Accepted = tuple.Generic.ModName.IndexOf(this.ModsFilter, StringComparison.InvariantCultureIgnoreCase) >= 0;
         }
 
         private void PopulateDependencies()
@@ -57,17 +77,6 @@ namespace Reloaded.Mod.Launcher.Pages.BaseSubpages.Dialogs
                 bool enabled = CurrentMod.ModConfig.ModDependencies.Contains(mod.ModConfig.ModId);
                 Dependencies.Add(new BooleanGenericTuple<IModConfig>(enabled, mod.ModConfig));
             }
-        }
-
-        private void OnClosed(object sender, EventArgs e)
-        {
-            CurrentMod.ModConfig.ModDependencies = Dependencies.Where(x => x.Enabled).Select(x => x.Generic.ModId).ToArray();
-            CurrentMod.Save();
-        }
-
-        private void ModsFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-            _dependenciesViewSource.View.Refresh();
         }
     }
 }
