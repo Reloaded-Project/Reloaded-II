550be231fbfc94d546290cbcb816bcaaaa46a19d
Sewer <seweratwork@gmail.com>
2020-01-02 10:15:08 +0000

    Make PropertyGrid ignore theme!
    .

---
+++ Source/Reloaded.Mod.Launcher/Controls/NoInheritanceContentControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Reloaded.Mod.Launcher.Controls
+{
+    public class NoInheritanceContentControl : ContentControl
+    {
+        //  https://stackoverflow.com/a/2138114
+        //  but here skip to theme instead of skipping all
+        //  otherwise the SearchPanel is rendered completely translucent
+        //
+        public NoInheritanceContentControl()
+        {
+            this.InheritanceBehavior = InheritanceBehavior.SkipToThemeNow;
+        }
+    }
+}
