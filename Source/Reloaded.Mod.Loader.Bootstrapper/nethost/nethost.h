b2ac7112f8a45c950992c019bc9a08233c00838d
Sewer56 <seweratwork@gmail.com>
2020-07-12 21:06:27 +0100

    Update nethost library for bootstrapping Reloaded

--- Source/Reloaded.Mod.Loader.Bootstrapper/nethost/nethost.h
+++ Source/Reloaded.Mod.Loader.Bootstrapper/nethost/nethost.h
@@ -5,20 +5,26 @@
 #ifndef __NETHOST_H__
 #define __NETHOST_H__
 
-#include <cstddef>
+#include <stddef.h>
 
-#if defined(_WIN32)
+#ifdef _WIN32
     #ifdef NETHOST_EXPORT
         #define NETHOST_API __declspec(dllexport)
     #else
-        #define NETHOST_API __declspec(dllimport)
+        // Consuming the nethost as a static library
+        // Shouldn't export attempt to dllimport.
+        #ifdef NETHOST_USE_AS_STATIC
+            #define NETHOST_API 
+        #else
+            #define NETHOST_API __declspec(dllimport)
+        #endif
     #endif
 
     #define NETHOST_CALLTYPE __stdcall
     #ifdef _WCHAR_T_DEFINED
-        using char_t = wchar_t;
+        typedef wchar_t char_t;
     #else
-        using char_t = unsigned short;
+        typedef unsigned short char_t;
     #endif
 #else
     #ifdef NETHOST_EXPORT
@@ -28,8 +34,34 @@
     #endif
 
     #define NETHOST_CALLTYPE
-    using char_t = char;
+    typedef char char_t;
 #endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Parameters for get_hostfxr_path
+//
+// Fields:
+//   size
+//     Size of the struct. This is used for versioning.
+//
+//   assembly_path
+//     Path to the compenent's assembly.
+//     If specified, hostfxr is located as if the assembly_path is the apphost
+//
+//   dotnet_root
+//     Path to directory containing the dotnet executable.
+//     If specified, hostfxr is located as if an application is started using
+//     'dotnet app.dll', which means it will be searched for under the dotnet_root
+//     path and the assembly_path is ignored.
+//
+struct get_hostfxr_parameters {
+    size_t size;
+    const char_t *assembly_path;
+    const char_t *dotnet_root;
+};
 
 //
 // Get the path to the hostfxr library
@@ -44,11 +76,9 @@
 //           or buffer is nullptr, this is populated with the minimum required size
 //           in char_t units for a buffer to hold the hostfxr path
 //
-//   assembly_path
-//     Optional. Path to the compenent's assembly. Whether or not this is specified
-//     determines the behaviour for locating the hostfxr library.
+//   get_hostfxr_parameters
+//     Optional. Parameters that modify the behaviour for locating the hostfxr library.
 //     If nullptr, hostfxr is located using the enviroment variable or global registration
-//     If specified, hostfxr is located as if the assembly_path is the apphost
 //
 // Return value:
 //   0 on success, otherwise failure
@@ -58,9 +88,13 @@
 //   The full search for the hostfxr library is done on every call. To minimize the need
 //   to call this function multiple times, pass a large buffer (e.g. PATH_MAX).
 //
-extern "C" NETHOST_API int NETHOST_CALLTYPE get_hostfxr_path(
+NETHOST_API int NETHOST_CALLTYPE get_hostfxr_path(
     char_t * buffer,
     size_t * buffer_size,
-    const char_t * assembly_path);
+    const struct get_hostfxr_parameters *parameters);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif // __NETHOST_H__
