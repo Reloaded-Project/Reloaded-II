a5494157a3bc0bfe14fcf8b197aec5317f4d0b74
Sewer56 <seweratwork@gmail.com>
2020-09-02 20:48:26 +0100

    Fixes for Framework Detection & Loader for x86

--- Source/Reloaded.Mod.Launcher/Utility/ProcessExtensions.cs
+++ Source/Reloaded.Mod.Launcher/Utility/ProcessExtensions.cs
@@ -8,8 +8,10 @@ namespace Reloaded.Mod.Launcher.Utility
     {
         public static bool Is64Bit(this Process process)
         {
-            IsWow64Process(process.Handle, out bool isGame32Bit);
-            return !isGame32Bit;
+            if (IntPtr.Size == 4)
+                return false;
+
+            return !(IsWow64Process(process.Handle, out bool isGame32Bit) && isGame32Bit);
         }
 
         [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
