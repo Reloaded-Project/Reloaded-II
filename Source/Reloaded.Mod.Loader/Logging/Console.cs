191f7e0e73091fae429adbb5676d8811362ae794
Sewer56 <seweratwork@gmail.com>
2020-09-06 13:28:46 +0100

    [Loader] Console: Misc Cleanup

--- Source/Reloaded.Mod.Loader/Logging/Console.cs
+++ Source/Reloaded.Mod.Loader/Logging/Console.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
@@ -47,30 +47,29 @@ namespace Reloaded.Mod.Loader.Logging
         }
 
         // Default Colours
-        public Color BackgroundColor    { get; set; } = Color.FromArgb(20, 25, 31);
-        public Color TextColor          { get; set; } = Color.FromArgb(239, 240, 235);
+        public Color BackgroundColor     { get; set; } = Color.FromArgb(20, 25, 31);
+        public Color TextColor           { get; set; } = Color.FromArgb(239, 240, 235);
 
-        public Color ColorRed           { get; set; } = Color.FromArgb(255, 92, 87);
-        public Color ColorRedLight      { get; set; } = Color.FromArgb(220, 163, 163);
+        public Color ColorRed            { get; set; } = Color.FromArgb(255, 92, 87);
+        public Color ColorRedLight       { get; set; } = Color.FromArgb(220, 163, 163);
 
-        public Color ColorGreen         { get; set; } = Color.FromArgb(90, 247, 142);
-        public Color ColorGreenLight    { get; set; } = Color.FromArgb(195, 191, 159);
+        public Color ColorGreen          { get; set; } = Color.FromArgb(90, 247, 142);
+        public Color ColorGreenLight     { get; set; } = Color.FromArgb(195, 191, 159);
 
-        public Color ColorYellow        { get; set; } = Color.FromArgb(243, 249, 157);
-        public Color ColorYellowLight   { get; set; } = Color.FromArgb(240, 223, 175);
+        public Color ColorYellow         { get; set; } = Color.FromArgb(243, 249, 157);
+        public Color ColorYellowLight    { get; set; } = Color.FromArgb(240, 223, 175);
 
-        public Color ColorBlue          { get; set; } = Color.FromArgb(87, 199, 255);
-        public Color ColorBlueLight     { get; set; } = Color.FromArgb(148, 191, 243);
+        public Color ColorBlue           { get; set; } = Color.FromArgb(87, 199, 255);
+        public Color ColorBlueLight      { get; set; } = Color.FromArgb(148, 191, 243);
 
-        public Color ColorPink          { get; set; } = Color.FromArgb(255, 106, 193);
-        public Color ColorPinkLight     { get; set; } = Color.FromArgb(236, 147, 211);
+        public Color ColorPink           { get; set; } = Color.FromArgb(255, 106, 193);
+        public Color ColorPinkLight      { get; set; } = Color.FromArgb(236, 147, 211);
 
-        public Color ColorLightBlue     { get; set; } = Color.FromArgb(154, 237, 254);
+        public Color ColorLightBlue      { get; set; } = Color.FromArgb(154, 237, 254);
         public Color ColorLightBlueLight { get; set; } = Color.FromArgb(147, 224, 227);
 
         /* Default Banner */
-
-        #region Print Banner
+        #region Banner: Reloaded Branding
         public void PrintBanner()
         {
             System.Console.Write("\n\n");
@@ -87,7 +86,6 @@ namespace Reloaded.Mod.Loader.Logging
                 new Formatter(@" :MMMm `MMMM.  ", ColorRed),
                 new Formatter(@" yMMMo /MMMd   ", ColorRed),
                 new Formatter(@"`MMMM. dMMM+   ", ColorRed),
-
             };
 
             string[] template =
@@ -154,9 +152,6 @@ namespace Reloaded.Mod.Loader.Logging
 
             System.Console.SetCursorPosition(consolePointer, System.Console.CursorTop);
         }
-
-
         #endregion
-
     }
 }
