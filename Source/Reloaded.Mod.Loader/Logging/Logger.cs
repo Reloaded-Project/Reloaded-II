2f6e199767bbf6da1c476706bceb05efca8239ac
Sewer56 <seweratwork@gmail.com>
2020-10-03 00:56:53 +0100

    Logger: Implement Full Log Exporting
    .

--- Source/Reloaded.Mod.Loader/Logging/Logger.cs
+++ Source/Reloaded.Mod.Loader/Logging/Logger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace Reloaded.Mod.Loader.Logging
 {
@@ -9,15 +10,16 @@ namespace Reloaded.Mod.Loader.Logging
     /// </summary>
     public class Logger : IDisposable
     {
-        private const int MaxLogLength = 1000;
+        private const int MaxBufferLength = 1000;
 
         /// <summary>
         /// Gets the path to which the log will be flushed to.
         /// </summary>
         public string FlushPath { get; }
 
-        private Console _console;
-        private Queue<string> _log;
+        private StreamWriter  _textStream;
+        private Console       _console;
+        private List<string>  _logItems;
 
         /// <summary>
         /// Intercepts logs from the console and provides the ability to flush them to a file in the event of a crash.
@@ -26,22 +28,37 @@ namespace Reloaded.Mod.Loader.Logging
         /// <param name="outputDir">The directory to which the log is output to.</param>
         public Logger(Console console, string outputDir)
         {
-            _log = new Queue<string>(MaxLogLength);
+            _logItems = new List<string>(MaxBufferLength + 1);
             _console = console;
-            _console.OnPrintMessage += OnPrintMessge;
-            FlushPath = Path.Combine(outputDir, "LoaderCrashLog.txt");
+            _console.OnPrintMessage += OnPrintMessage;
+            FlushPath = Path.Combine(outputDir, "ReloadedLog.txt");
+            _textStream = File.CreateText(FlushPath);
+            _textStream.AutoFlush = false;
         }
 
-        public void Flush() => File.WriteAllLines(FlushPath, _log);
-        private void OnPrintMessge(object sender, string message)
+        /// <summary>
+        /// Flushes the current contents of the log.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void Flush()
         {
-            if (_log.Count == MaxLogLength)
-                _log.Dequeue();
+            foreach (var item in _logItems) 
+                _textStream.WriteLine(item);
 
-            _log.Enqueue(message);
+            _textStream.Flush();
+            _logItems.Clear();
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void OnPrintMessage(object sender, string message)
+        {
+            if (_logItems.Count == MaxBufferLength)
+                Flush();
+
+            _logItems.Add(message);
         }
 
         /// <inheritdoc />
-        public void Dispose() => _console.OnPrintMessage -= OnPrintMessge;
+        public void Dispose() => _console.OnPrintMessage -= OnPrintMessage;
     }
 }
