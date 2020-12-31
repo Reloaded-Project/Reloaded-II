a2c096ce157b8bb3b9073ad9e48cbae13c1731b8
Sewer56 <seweratwork@gmail.com>
2020-12-31 16:37:07 +0000

    NuGetConverter: Set file path not folder path.
    .

--- source/Tools/NuGetConverter/Program.cs
+++ source/Tools/NuGetConverter/Program.cs
@@ -11,23 +11,22 @@ namespace NuGetConverter
             Console.WriteLine("Reloaded-II NuGet Package Converter\n" +
                               "Converts mod folders or archives into NuGet packages\n" +
                               "Usage: NuGetConverter.exe <Mod Folder or Archive Path> <Output Path>\n" +
-                              "Example: NuGetConverter.exe Mod.zip .\n" +
-                              "Example: NuGetConverter.exe reloaded.test.mod .\n" +
-                              "Example: NuGetConverter.exe reloaded.text.mod ./packages/");
+                              "Example: NuGetConverter.exe Mod.zip Mod.nupkg\n" +
+                              "Example: NuGetConverter.exe reloaded.test.mod reloaded.test.mod.nupkg\n" +
+                              "Example: NuGetConverter.exe reloaded.test.mod ./packages/reloaded.test.mod.nupkg");
 
             var input  = args[0];
             var output = args[1];
             if (File.GetAttributes(input).HasFlag(FileAttributes.Directory))
-                Move(Converter.FromModDirectory(input, output), output);
+                Move(Converter.FromModDirectory(input, Path.GetDirectoryName(output)), Path.GetFullPath(output));
             else
-                Move(Converter.FromArchiveFile(input, output), output);
+                Move(Converter.FromArchiveFile(input, Path.GetDirectoryName(output)), Path.GetFullPath(output));
         }
 
-        static void Move(string filePath, string outputFolder)
+        static void Move(string filePath, string outputPath)
         {
-            var newPath = Path.Combine(outputFolder, Path.GetFileName(filePath));
-            Console.WriteLine($"Writing to: {newPath}");
-            File.Move(filePath, newPath, true);
+            Console.WriteLine($"Writing to: {outputPath}");
+            File.Move(filePath, outputPath, true);
         }
     }
 }
