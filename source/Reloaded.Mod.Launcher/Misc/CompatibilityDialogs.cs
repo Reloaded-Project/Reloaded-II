7ff28e2b0caf940d234993b43b4f12bbbe129445
Sewer56 <seweratwork@gmail.com>
2021-04-10 00:13:13 +0100

    Added: Compatibiility Notice for Wine Users

---
+++ source/Reloaded.Mod.Launcher/Misc/CompatibilityDialogs.cs
@@ -0,0 +1,16 @@
+﻿using Reloaded.Mod.Launcher.Pages.Dialogs;
+
+namespace Reloaded.Mod.Launcher.Misc
+{
+    public static class CompatibilityDialogs
+    {
+        public static bool WineShowLaunchDialog()
+        {
+            var wineDialog = new XamlResourceMessageBoxOkCancel("WineCompatibilityNoticeTitle", "WineCompatibilityNoticeText", "WineCompatibilityNoticeOk", "MessageBoxButtonCancel");
+            wineDialog.MaxWidth = 680;
+
+            var result = wineDialog.ShowDialog();
+            return result.HasValue && result.Value;
+        }
+    }
+}
