272a528b02b685002c1a50fd1be6a494aeb42c9f
Sewer 56 <seweratwork@gmail.com>
2021-11-25 03:55:49 +0000

    Added: Extension to convert string,object back to Type <T>

--- source/Reloaded.Mod.Loader.IO/Utility/Extensions.cs
+++ source/Reloaded.Mod.Loader.IO/Utility/Extensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 
 namespace Reloaded.Mod.Loader.IO.Utility
@@ -14,6 +17,27 @@ namespace Reloaded.Mod.Loader.IO.Utility
                 context.Post(state => action(), null);
             else
                 action();
+        }
+
+        /// <summary>
+        /// Retrieves an item (which may have been deserialized) from a string->object map.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pluginData">The dictionary to get the item from.</param>
+        /// <param name="key">The item's key.</param>
+        /// <returns>The item in question, or default if it does not exist.</returns>
+        public static T TryGetValue<T>(this Dictionary<string, object> pluginData, string key)
+        {
+            if (!pluginData.TryGetValue(key, out var value))
+                return default;
+
+            if (value is JsonElement element)
+                return JsonSerializer.Deserialize<T>(element.ToString());
+
+            if (value is T result)
+                return result;
+
+            throw new Exception($"Cannot convert key from dictionary to specified type T ({nameof(T)})");
         }
     }
 }
