b28e110784fe80c685e2f84d8986e69042898225
Sewer 56 <seweratwork@gmail.com>
2021-12-16 13:08:37 +0000

    Fixed: Old Config Read/Write Test.

--- source/Reloaded.Mod.Loader.Tests/IO/LoaderConfigReaderTest.cs
+++ source/Reloaded.Mod.Loader.Tests/IO/LoaderConfigReaderTest.cs
@@ -28,8 +28,18 @@ namespace Reloaded.Mod.Loader.Tests.IO
             IConfig<LoaderConfig>.ToPath(config, Paths.LoaderConfigPath);
             var newConfig = IConfig<LoaderConfig>.FromPath(Paths.LoaderConfigPath);
 
-            // Restore old config and assert.
-            Assert.Equal(config, newConfig);
+            // Restore old config and assert some members.
+            Assert.Equal(config.LoaderPath32, newConfig.LoaderPath32);
+            Assert.Equal(config.LoaderPath64, newConfig.LoaderPath64);
+            Assert.Equal(config.LauncherPath, newConfig.LauncherPath);
+            Assert.Equal(config.Bootstrapper32Path, newConfig.Bootstrapper32Path);
+            Assert.Equal(config.Bootstrapper64Path, newConfig.Bootstrapper64Path);
+            Assert.Equal(config.ApplicationConfigDirectory, newConfig.ApplicationConfigDirectory);
+            Assert.Equal(config.ModUserConfigDirectory, newConfig.ModUserConfigDirectory);
+            Assert.Equal(config.PluginConfigDirectory, newConfig.PluginConfigDirectory);
+            Assert.Equal(config.ModConfigDirectory, newConfig.ModConfigDirectory);
+            Assert.Equal(config.LanguageFile, newConfig.LanguageFile);
+            Assert.Equal(config.ThemeFile, newConfig.ThemeFile);
         }
 
         /* Check for valid directory test. */
