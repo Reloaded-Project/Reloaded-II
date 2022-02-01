59aef39301d6c15814a22cf88f061745ad84abc0
Sewer 56 <seweratwork@gmail.com>
2022-02-01 04:37:56 +0000

    Added: Support for GitHub Dependency Resolution

---
+++ source/Reloaded.Mod.Loader.Tests/Update/Providers/GitHub/GitHubDependencyResolverTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using Reloaded.Mod.Loader.IO.Config;
+using Reloaded.Mod.Loader.Tests.SETUP;
+using Reloaded.Mod.Loader.Update.Interfaces;
+using Reloaded.Mod.Loader.Update.Providers.GameBanana;
+using Reloaded.Mod.Loader.Update.Providers.GitHub;
+using Sewer56.DeltaPatchGenerator.Lib.Utility;
+using Sewer56.Update.Misc;
+using Xunit;
+
+namespace Reloaded.Mod.Loader.Tests.Update.Providers.GitHub;
+
+public class GitHubDependencyResolverTests : IDisposable
+{
+    /* Initialize/Dispose to protect existing config. */
+    private TestEnvironmoent _testEnvironmoent = new TestEnvironmoent();
+
+    public void Dispose()
+    {
+        _testEnvironmoent.Dispose();
+    }
+
+    [Fact]
+    public async Task ResolveAsync_WithExistingPackage_ReturnsPackages()
+    {
+        // Arrange 
+        var config = new GitHubReleasesUpdateResolverFactory.GitHubConfig()
+        {
+            RepositoryName = "Update.Test.Repo",
+            UserName = "Sewer56"
+        };
+        var clonedDependency = _testEnvironmoent.TestModConfigBTuple.DeepClone();
+        Singleton<GitHubReleasesUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, config);
+        var clonedOriginal   = _testEnvironmoent.TestModConfigATuple.DeepClone();
+        
+        var gitHub = new GitHubReleasesDependencyMetadataWriter();
+        gitHub.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
+
+        // Act
+        var resolver = new GitHubDependencyResolver();
+        var result = await resolver.ResolveAsync(clonedDependency.Config.ModId, clonedOriginal.Config);
+
+        // Assert
+        Assert.NotEmpty(result.FoundDependencies);
+        Assert.Empty(result.NotFoundDependencies);
+    }
+
+    [Fact]
+    public async Task ResolveAsync_WithExistingPackage_CanDownloadPackage()
+    {
+        // Arrange
+        var config = new GitHubReleasesUpdateResolverFactory.GitHubConfig()
+        {
+            RepositoryName = "reloaded.universal.redirector",
+            UserName = "Reloaded-Project",
+            AssetFileName = "reloaded.universal.monitor.zip"
+        };
+        var clonedDependency = _testEnvironmoent.TestModConfigBTuple.DeepClone();
+        Singleton<GitHubReleasesUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, config);
+        var clonedOriginal = _testEnvironmoent.TestModConfigATuple.DeepClone();
+
+        var gameBanana = new GitHubReleasesDependencyMetadataWriter();
+        gameBanana.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
+
+        // Act
+        using var outputDirectory = new TemporaryFolderAllocation();
+        var resolver = new GitHubDependencyResolver();
+        var result   = await resolver.ResolveAsync(clonedDependency.Config.ModId, clonedOriginal.Config);
+        var downloadedPackagePath = await result.FoundDependencies[0].DownloadAsync(outputDirectory.FolderPath, null);
+
+        // Assert
+        Assert.True(Directory.Exists(downloadedPackagePath));
+        Assert.True(File.Exists(Path.Combine(downloadedPackagePath, ModConfig.ConfigFileName)));
+    }
+
+    [Fact]
+    public async Task ResolveAsync_WithNoPackage_ReturnsMissing()
+    {
+        // Arrange
+        var resolver = new GitHubDependencyResolver();
+
+        // Act
+        var result = await resolver.ResolveAsync("this.package.does.not.exist");
+
+        // Assert
+        Assert.NotEmpty(result.NotFoundDependencies);
+        Assert.Empty(result.FoundDependencies);
+    }
+}
---
+++ source/Reloaded.Mod.Loader.Update/Providers/GitHub/GitHubDependencyResolver.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Reloaded.Mod.Interfaces.Utilities;
+using Reloaded.Mod.Loader.IO.Config;
+using Reloaded.Mod.Loader.Update.Interfaces;
+using Reloaded.Mod.Loader.Update.Providers.Update;
+using Sewer56.Update.Interfaces;
+using Sewer56.Update.Resolvers.GitHub;
+using Sewer56.Update.Structures;
+
+namespace Reloaded.Mod.Loader.Update.Providers.GitHub;
+
+/// <summary>
+/// Resolves for missing GitHub dependencies.
+/// </summary>
+public class GitHubDependencyResolver : IDependencyResolver
+{
+    /// <inheritdoc />
+    public async Task<ModDependencyResolveResult> ResolveAsync(string packageId, ModConfig? modConfig = null, CancellationToken token = default)
+    {        
+        // If no mod config is provided, we cannot resolve.
+        if (modConfig == null)
+            return new ModDependencyResolveResult() { NotFoundDependencies = { packageId } };
+
+        // If no dependency data is available, return none.
+        if (!modConfig.PluginData.TryGetValue(GitHubReleasesDependencyMetadataWriter.PluginId, out DependencyResolverMetadata<GitHubReleasesUpdateResolverFactory.GitHubConfig> metadata))
+            return new ModDependencyResolveResult() { NotFoundDependencies = { packageId } };
+
+        // Try to get configuration for update.
+        if (!metadata.IdToConfigMap.TryGetValue(packageId, out var gitConfig))
+            return new ModDependencyResolveResult() { NotFoundDependencies = { packageId } };
+
+        var result = new ModDependencyResolveResult();
+        var resolver = new GitHubReleaseResolver(new GitHubResolverConfiguration()
+        {
+            UserName = gitConfig.Config.UserName,
+            RepositoryName = gitConfig.Config.RepositoryName,
+            LegacyFallbackPattern = gitConfig.Config.AssetFileName,
+            
+        }, new CommonPackageResolverSettings() { MetadataFileName = gitConfig.ReleaseMetadataName });
+
+        await ((IPackageResolver)resolver).InitializeAsync();
+        result.FoundDependencies.Add(new UpdateDownloadablePackage(resolver)
+        {
+            Id = packageId
+        });
+
+        return result;
+    }
+}
