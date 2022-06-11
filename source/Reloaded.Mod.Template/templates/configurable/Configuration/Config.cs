5a2f97bf12d4a9d0ae24d56b4b737c10ae2cfafa
Sewer 56 <seweratwork@gmail.com>
2022-06-11 23:26:19 +0100

    Added: New Mod Template

---
+++ source/Reloaded.Mod.Template/templates/configurable/Configuration/Config.cs
@@ -0,0 +1,48 @@
+﻿using Reloaded.Mod.Template.Configuration.Implementation;
+using System.ComponentModel;
+
+namespace Reloaded.Mod.Template.Configuration
+{
+    public class Config : Configurable<Config>
+    {
+        /*
+            User Properties:
+                - Please put all of your configurable properties here.
+                - Tip: Consider using the various available attributes https://stackoverflow.com/a/15051390/11106111
+        
+            By default, configuration saves as "Config.json" in mod folder.    
+            Need more config files/classes? See Configuration.cs
+        */
+
+
+        [DisplayName("String")]
+        [Description("This is a string.")]
+        public string String { get; set; } = "Default Name";
+
+        [DisplayName("Int")]
+        [Description("This is an int.")]
+        public int Integer { get; set; } = 42;
+
+        [DisplayName("Bool")]
+        [Description("This is a bool.")]
+        public bool Boolean { get; set; } = true;
+
+        [DisplayName("Float")]
+        [Description("This is a floating point number.")]
+        public float Float { get; set; } = 6.987654F;
+
+        [DisplayName("Enum")]
+        [Description("This is an enumerable.")]
+        public SampleEnum Reloaded { get; set; } = SampleEnum.ILoveIt;
+
+        public enum SampleEnum
+        {
+            NoOpinion,
+            Sucks,
+            IsMediocre,
+            IsOk,
+            IsCool,
+            ILoveIt
+        }
+    }
+}
---
+++ source/Reloaded.Mod.Template/templates/configurable/Mod.cs
@@ -0,0 +1,15 @@
+﻿using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+
+namespace Reloaded.Mod.Template;
+
+/// <summary>
+/// Your mod logic goes here.
+/// </summary>
+public class Mod
+{
+    public Mod(IReloadedHooks hooks, ILogger logger)
+    {
+        // TODO: Implement some mod logic    
+    }
+}
