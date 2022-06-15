4897f1166cb299753bc4e7e97c5ca8222a56077c
Sewer 56 <seweratwork@gmail.com>
2022-06-15 03:33:14 +0100

    Added: Tests to Ensure Equals() is implemented Properly for Resolver Configs

--- source/Reloaded.Mod.Loader.Tests/Update/DependencyMetadataWriterTests.cs
+++ source/Reloaded.Mod.Loader.Tests/Update/DependencyMetadataWriterTests.cs
@@ -103,23 +103,46 @@ public class DependencyMetadataWriterTests : IDisposable
     }
 
     [Fact]
+    public void WithNoDependencies_RemovesPluginData_UsingGitHub()
+    {
+        // Arrange 
+        var config = new GitHubReleasesUpdateResolverFactory.GitHubConfig() { AssetFileName = "Deeznutz" };
+        var gitHub = new GitHubReleasesDependencyMetadataWriter();
+        WithNoDependencies_RemovesPluginData_Base<GitHubReleasesUpdateResolverFactory.GitHubConfig, GitHubReleasesUpdateResolverFactory>(gitHub, config, GameBananaDependencyMetadataWriter.PluginId);
+    }
+
+    [Fact]
     public void WithNoDependencies_RemovesPluginData_UsingGameBanana()
     {
         // Arrange 
         var config = new GameBananaUpdateResolverFactory.GameBananaConfig() { ItemId = 6969 };
+        var gameBanana = new GameBananaDependencyMetadataWriter();
+        WithNoDependencies_RemovesPluginData_Base<GameBananaUpdateResolverFactory.GameBananaConfig, GameBananaUpdateResolverFactory>(gameBanana, config, GameBananaDependencyMetadataWriter.PluginId);
+    }
+    
+    private void WithNoDependencies_RemovesPluginData_Base<TConfigType, TUpdateResolverFactory>(IDependencyMetadataWriter metadataWriter, TConfigType config, string pluginId) where TUpdateResolverFactory : IUpdateResolverFactory, new()
+    {
+        // Arrange 
         var clonedDependency = _testEnvironmoent.TestModConfigBTuple.DeepClone();
-        Singleton<GameBananaUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, config);
+        Singleton<TUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, config);
         var clonedOriginal = _testEnvironmoent.TestModConfigATuple.DeepClone();
 
         // Act
-        var gameBanana = new GameBananaDependencyMetadataWriter();
-        gameBanana.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
-
-        Singleton<GameBananaUpdateResolverFactory>.Instance.RemoveConfiguration(clonedDependency);
-        gameBanana.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
+        metadataWriter.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
+        Singleton<TUpdateResolverFactory>.Instance.RemoveConfiguration(clonedDependency); // Remove update info from dependency
+        metadataWriter.Update(clonedOriginal.Config, new[] { clonedDependency.Config });  // Since dependency has no updates, should be removed from here too
 
         // Assert
-        Assert.False(clonedOriginal.Config.PluginData.ContainsKey(GameBananaDependencyMetadataWriter.PluginId));
+        Assert.False(clonedOriginal.Config.PluginData.ContainsKey(pluginId));
+    }
+
+    [Fact]
+    public void WithChangedDependency_ReturnsTrue_UsingGitHub()
+    {
+        // Arrange 
+        var configOld = new GitHubReleasesUpdateResolverFactory.GitHubConfig() { RepositoryName = "OldRepoName" };
+        var configNew = new GitHubReleasesUpdateResolverFactory.GitHubConfig() { RepositoryName = "NewRepoName" };
+        WithChangedDependency_ReturnsTrue_Base<GitHubReleasesUpdateResolverFactory.GitHubConfig, GitHubReleasesUpdateResolverFactory>(new GitHubReleasesDependencyMetadataWriter(), configOld, configNew);
     }
 
     [Fact]
@@ -128,17 +151,24 @@ public class DependencyMetadataWriterTests : IDisposable
         // Arrange 
         var configOld = new GameBananaUpdateResolverFactory.GameBananaConfig() { ItemId = 6969 };
         var configNew = new GameBananaUpdateResolverFactory.GameBananaConfig() { ItemId = 1337 };
+        WithChangedDependency_ReturnsTrue_Base<GameBananaUpdateResolverFactory.GameBananaConfig, GameBananaUpdateResolverFactory>(new GameBananaDependencyMetadataWriter(), configOld, configNew);
+    }
+    
+    private void WithChangedDependency_ReturnsTrue_Base<TConfigType, TUpdateResolverFactory>(IDependencyMetadataWriter metadataWriter, TConfigType configOld, TConfigType configNew) where TUpdateResolverFactory : IUpdateResolverFactory, new()
+    {
+        // Assert that Equals is properly implemented.
+        Assert.Equal(configNew, configNew.DeepClone());
 
+        // Arrange 
         var clonedDependency = _testEnvironmoent.TestModConfigBTuple.DeepClone();
-        Singleton<GameBananaUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, configOld);
+        Singleton<TUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, configOld);
         var clonedOriginal = _testEnvironmoent.TestModConfigATuple.DeepClone();
 
         // Act
-        var gameBanana = new GameBananaDependencyMetadataWriter();
-        gameBanana.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
+        metadataWriter.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
 
-        Singleton<GameBananaUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, configNew);
-        bool shouldUpdateFile = gameBanana.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
+        Singleton<TUpdateResolverFactory>.Instance.SetConfiguration(clonedDependency, configNew);
+        bool shouldUpdateFile = metadataWriter.Update(clonedOriginal.Config, new[] { clonedDependency.Config });
 
         // Assert
         Assert.True(shouldUpdateFile);
