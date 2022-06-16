bb06b86c375779c22c1b9d9796375821816bbcd6
Sewer 56 <seweratwork@gmail.com>
2022-06-16 05:38:25 +0100

    Added: Script for Building Mod with IL Linker

---
+++ source/Reloaded.Mod.Template/templates/configurable/BuildLinked.ps1
@@ -0,0 +1,9 @@
+# Set Working Directory
+Split-Path $MyInvocation.MyCommand.Path | Push-Location
+[Environment]::CurrentDirectory = $PWD
+
+Remove-Item "$env:RELOADEDIIMODS/Reloaded.Mod.Template/*" -Force -Recurse
+dotnet publish "./Reloaded.Mod.Template.csproj" -c Release -o "$env:RELOADEDIIMODS/Reloaded.Mod.Template" /p:OutputPath="./bin/Release" /p:RobustILLink="true"
+
+# Restore Working Directory
+Pop-Location
