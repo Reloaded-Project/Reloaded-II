16e9d89b3d8cdf11d2e90448c8aaf6e12472d5e8
Sewer 56 <seweratwork@gmail.com>
2022-06-17 11:24:30 +0100

    Changed: Remove MessagePack from the Server

--- source/Reloaded.Mod.Loader.Server/Messages/Server/SuspendMod.cs
+++ source/Reloaded.Mod.Loader.Server/Messages/Server/SuspendMod.cs
@@ -1,12 +1,13 @@
-﻿using Reloaded.Messaging.Interfaces;
-using Reloaded.Messaging.Serializer.MessagePack;
+﻿using System.Text.Json;
+using Reloaded.Messaging.Interfaces;
+using Reloaded.Messaging.Serializer.SystemTextJson;
 
 namespace Reloaded.Mod.Loader.Server.Messages.Server;
 
 public struct SuspendMod : IMessage<MessageType>
 {
     public MessageType GetMessageType() => MessageType.SuspendMod;
-    public ISerializer GetSerializer() => new MsgPackSerializer(true);
+    public ISerializer GetSerializer() => new SystemTextJsonSerializer(JsonSerializerOptions.Default);
     public ICompressor GetCompressor() => null;
 
     public string ModId { get; set; }
