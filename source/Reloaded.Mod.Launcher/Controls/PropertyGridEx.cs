25f68bb86295690d1aa2455f3babb0eb69acb396
Sewer 56 <seweratwork@gmail.com>
2022-06-21 12:27:50 +0100

    Improved: Mouse detection & tooltip speed in Configure Mods menu.

--- source/Reloaded.Mod.Launcher/Controls/PropertyGridEx.cs
+++ source/Reloaded.Mod.Launcher/Controls/PropertyGridEx.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Reflection;
-using System.Reflection.Emit;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows.Markup;
-using System.Xml;
+using System.Windows.Media;
 using HandyControl.Controls;
-using HandyControl.Tools;
 using Reloaded.Mod.Launcher.Lib.Commands.General;
 using Reloaded.Mod.Loader.Update.Utilities;
 using TextBox = System.Windows.Controls.TextBox;
@@ -80,15 +75,101 @@ public class PropertyResolverEx : PropertyResolver
 /// </summary>
 public static class PropertyResolverExtensions 
 {
-    public static object? GetTooltip(this PropertyItem propertyItem)
+    public static void AttachTooltipAdder(this PropertyItem propertyItem)
     {
         if (string.IsNullOrEmpty(propertyItem.Description))
-            return null;
+            return;
+
+        propertyItem.Loaded += PropertyItemLoaded;
+    }
+
+    private static void PropertyItemLoaded(object? sender, EventArgs e)
+    {
+        var propertyItem = (PropertyItem)sender!;
+        var textbox = FindChild<TextBox>(propertyItem, "");
+        if (textbox != null)
+            textbox.ToolTip = null;
 
         var tooltip = new ToolTip();
         tooltip.Content = propertyItem.Description;
         ToolTipService.SetInitialShowDelay(tooltip, 0);
-        return tooltip;
+        ToolTipService.SetBetweenShowDelay(tooltip, 0);
+        propertyItem.ToolTip = tooltip;
+        propertyItem.MouseEnter += PropertyItemOnMouseEnter;
+        propertyItem.MouseLeave += PropertyItemOnMouseLeave;
+
+        // Set grid colour to transparent so it's hit testable and can pick up mouse events
+        var groupbox = FindChild<Grid>(propertyItem, "");
+        if (groupbox != null)
+            groupbox.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+    }
+
+    private static void PropertyItemOnMouseEnter(object sender, MouseEventArgs e)
+    {
+        var propertyItem = (PropertyItem)sender;
+        if (propertyItem.ToolTip is ToolTip tooltip)
+            tooltip.IsOpen = true;
+    }
+
+    private static void PropertyItemOnMouseLeave(object sender, MouseEventArgs e)
+    {
+        var propertyItem = (PropertyItem)sender;
+        if (propertyItem.ToolTip is ToolTip tooltip)
+            tooltip.IsOpen = false;
+    }
+
+    /// <summary>
+    /// Finds a Child of a given item in the visual tree. 
+    /// </summary>
+    /// <param name="parent">A direct parent of the queried item.</param>
+    /// <typeparam name="T">The type of the queried item.</typeparam>
+    /// <param name="childName">x:Name or Name of child. </param>
+    /// <returns>The first parent item that matches the submitted type parameter. 
+    /// If not matching item can be found, 
+    /// a null parent is being returned.</returns>
+    public static T? FindChild<T>(DependencyObject? parent, string? childName)
+        where T : DependencyObject
+    {
+        // Confirm parent and childName are valid. 
+        if (parent == null) 
+            return null;
+
+        T? foundChild = null;
+        int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+        for (int i = 0; i < childrenCount; i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+
+            // If the child is not of the request child type child
+            T? childType = child as T;
+            if (childType == null)
+            {
+                // recursively drill down the tree
+                foundChild = FindChild<T>(child, childName);
+
+                // If the child is found, break so we do not overwrite the found child. 
+                if (foundChild != null) 
+                    break;
+            }
+            else if (!string.IsNullOrEmpty(childName))
+            {
+                // If the child's name is set for search
+                if (child is not FrameworkElement frameworkElement || frameworkElement.Name != childName) 
+                    continue;
+
+                // if the child's name is of the request name
+                foundChild = (T)child;
+                break;
+            }
+            else
+            {
+                // child element found.
+                foundChild = (T)child;
+                break;
+            }
+        }
+
+        return foundChild;
     }
 }
 
@@ -96,15 +177,15 @@ public class StringWrapperEditor : PropertyEditorBase
 {
     public override FrameworkElement CreateElement(PropertyItem propertyItem)
     {
-        var dataGrid = new System.Windows.Controls.DataGrid
+        propertyItem.AttachTooltipAdder();
+        var dataGrid = new DataGrid
         {
-            ToolTip = propertyItem.GetTooltip(),
             AutoGenerateColumns = false,
             HeadersVisibility = DataGridHeadersVisibility.None,
             CanUserAddRows = false,
         };
 
-        dataGrid.SetResourceReference(Control.StyleProperty, "DefaultDataGrid");
+        dataGrid.SetResourceReference(FrameworkElement.StyleProperty, "DefaultDataGrid");
         dataGrid.Columns.Add(new DataGridTextColumn()
         {
             Binding = new Binding(nameof(StringWrapper.Value)) { Mode = BindingMode.TwoWay }
@@ -147,31 +228,34 @@ public class StringWrapperEditor : PropertyEditorBase
         return dataGrid;
     }
 
-    public override DependencyProperty GetDependencyProperty() => System.Windows.Controls.DataGrid.ItemsSourceProperty;
+    public override DependencyProperty GetDependencyProperty() => ItemsControl.ItemsSourceProperty;
 }
 
 public class PlainTextPropertyEditor : PropertyEditorBase
 {
     public override FrameworkElement CreateElement(PropertyItem propertyItem)
     {
-        return new System.Windows.Controls.TextBox
+        propertyItem.AttachTooltipAdder();
+        return new TextBox
         {
             IsReadOnly = propertyItem.IsReadOnly,
-            ToolTip = propertyItem.GetTooltip()
         };
     }
 
-    public override DependencyProperty GetDependencyProperty() => System.Windows.Controls.TextBox.TextProperty;
+    public override DependencyProperty GetDependencyProperty() => TextBox.TextProperty;
 }
 
 public class EnumPropertyEditor : PropertyEditorBase
 {
-    public override FrameworkElement CreateElement(PropertyItem propertyItem) => new System.Windows.Controls.ComboBox
+    public override FrameworkElement CreateElement(PropertyItem propertyItem)
     {
-        IsEnabled = !propertyItem.IsReadOnly,
-        ItemsSource = Enum.GetValues(propertyItem.PropertyType),
-        ToolTip = propertyItem.GetTooltip()
-    };
+        propertyItem.AttachTooltipAdder();
+        return new System.Windows.Controls.ComboBox
+        {
+            IsEnabled = !propertyItem.IsReadOnly,
+            ItemsSource = Enum.GetValues(propertyItem.PropertyType)
+        };
+    }
 
     public override DependencyProperty GetDependencyProperty() => Selector.SelectedValueProperty;
 }
@@ -190,13 +274,16 @@ public class NumberPropertyEditor : PropertyEditorBase
 
     public double Maximum { get; set; }
 
-    public override FrameworkElement CreateElement(PropertyItem propertyItem) => new NumericUpDown
+    public override FrameworkElement CreateElement(PropertyItem propertyItem)
     {
-        IsReadOnly = propertyItem.IsReadOnly,
-        Minimum = Minimum,
-        Maximum = Maximum,
-        ToolTip = propertyItem.GetTooltip()
-    };
+        propertyItem.AttachTooltipAdder();
+        return new NumericUpDown
+        {
+            IsReadOnly = propertyItem.IsReadOnly,
+            Minimum = Minimum,
+            Maximum = Maximum
+        };
+    }
 
     public override DependencyProperty GetDependencyProperty() => NumericUpDown.ValueProperty;
 }
@@ -206,7 +293,7 @@ public class SwitchPropertyEditorEx : SwitchPropertyEditor
     public override FrameworkElement CreateElement(PropertyItem propertyItem)
     {
         var result = base.CreateElement(propertyItem);
-        result.ToolTip = propertyItem.GetTooltip();
+        propertyItem.AttachTooltipAdder();
         return result;
     }
 
@@ -215,11 +302,14 @@ public class SwitchPropertyEditorEx : SwitchPropertyEditor
 
 public class DateTimePropertyEditor : PropertyEditorBase
 {
-    public override FrameworkElement CreateElement(PropertyItem propertyItem) => new DateTimePicker
+    public override FrameworkElement CreateElement(PropertyItem propertyItem)
     {
-        IsEnabled = !propertyItem.IsReadOnly,
-        ToolTip = propertyItem.GetTooltip()
-    };
+        propertyItem.AttachTooltipAdder();
+        return new DateTimePicker
+        {
+            IsEnabled = !propertyItem.IsReadOnly
+        };
+    }
 
     public override DependencyProperty GetDependencyProperty() => DateTimePicker.SelectedDateTimeProperty;
 }
