f1a326c66ef190dc8caede40bc93f9c3b5f31e73
Sewer 56 <seweratwork@gmail.com>
2022-06-23 13:05:13 +0100

    Updated: Mod Template Available Attributes.

--- source/Reloaded.Mod.Template/templates/configurable/Configuration/Config.cs
+++ source/Reloaded.Mod.Template/templates/configurable/Configuration/Config.cs
@@ -8,31 +8,47 @@ namespace Reloaded.Mod.Template.Configuration
         /*
             User Properties:
                 - Please put all of your configurable properties here.
-                - Tip: Consider using the various available attributes https://stackoverflow.com/a/15051390/11106111
         
-            By default, configuration saves as "Config.json" in mod folder.    
+            By default, configuration saves as "Config.json" in mod user config folder.    
             Need more config files/classes? See Configuration.cs
+        
+            Available Attributes:
+            - Category
+            - DisplayName
+            - Description
+            - DefaultValue
+
+            // Technically Supported but not Useful
+            - Browsable
+            - Localizable
+
+            The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
         */
 
 
         [DisplayName("String")]
         [Description("This is a string.")]
+        [DefaultValue("Default Name")]
         public string String { get; set; } = "Default Name";
 
         [DisplayName("Int")]
         [Description("This is an int.")]
+        [DefaultValue(42)]
         public int Integer { get; set; } = 42;
 
         [DisplayName("Bool")]
         [Description("This is a bool.")]
+        [DefaultValue(true)]
         public bool Boolean { get; set; } = true;
 
         [DisplayName("Float")]
         [Description("This is a floating point number.")]
+        [DefaultValue(6.987654F)]
         public float Float { get; set; } = 6.987654F;
 
         [DisplayName("Enum")]
         [Description("This is an enumerable.")]
+        [DefaultValue(SampleEnum.ILoveIt)]
         public SampleEnum Reloaded { get; set; } = SampleEnum.ILoveIt;
 
         public enum SampleEnum
