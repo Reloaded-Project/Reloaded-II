77c454db125eeebf283faad578fcc38e6237385f
Sewer 56 <seweratwork@gmail.com>
2022-07-05 14:25:52 +0100

    Bumped: Bootstrapper Version

--- source/Reloaded.Mod.Loader.Bootstrapper/EntryPointParameter.h
+++ source/Reloaded.Mod.Loader.Bootstrapper/EntryPointParameter.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 /* For C# source, see EntryPoint.cs */
-#define CURRENT_VERSION 1
+#define CURRENT_VERSION 3
 
 enum EntryPointFlags : int
 {
--- source/Reloaded.Mod.Shared/EntryPointParameters.cs
+++ source/Reloaded.Mod.Shared/EntryPointParameters.cs
@@ -14,7 +14,7 @@ public struct EntryPointParameters
     /// <summary>
     /// Current version of parameters.
     /// </summary>
-    public const int CurrentVersion = 1;
+    public const int CurrentVersion = 3;
 
     // Version 1
     public int Version;
