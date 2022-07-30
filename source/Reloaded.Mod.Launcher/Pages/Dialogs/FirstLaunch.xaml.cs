de7ebed7489c82d765b1b7035cf994bd76b7d28b
Sewer 56 <seweratwork@gmail.com>
2022-07-30 19:47:36 +0100

    Fixed: Individual Dialogs' Button Visibility doesn't affect Others

--- source/Reloaded.Mod.Launcher/Pages/Dialogs/FirstLaunch.xaml.cs
+++ source/Reloaded.Mod.Launcher/Pages/Dialogs/FirstLaunch.xaml.cs
@@ -6,11 +6,7 @@
 public partial class FirstLaunch : ReloadedWindow
 {
     public new FirstLaunchViewModel ViewModel { get; set; }
-
-    public Visibility OriginalCloseVisibility;
-    public Visibility OriginalMinimizeVisibility;
-    public Visibility OriginalMaximizeVisibility;
-
+    
     public FirstLaunch()
     {
         InitializeComponent();
@@ -19,23 +15,11 @@ public partial class FirstLaunch : ReloadedWindow
         ViewModel = Lib.IoC.Get<FirstLaunchViewModel>();
         
         // Disable Original Button Visibility
-        OriginalCloseVisibility = base.ViewModel.CloseButtonVisibility;
-        OriginalMinimizeVisibility = base.ViewModel.MinimizeButtonVisibility;
-        OriginalMaximizeVisibility = base.ViewModel.MaximizeButtonVisibility;
         base.ViewModel.CloseButtonVisibility = Visibility.Collapsed;
         base.ViewModel.MaximizeButtonVisibility = Visibility.Collapsed;
         base.ViewModel.MinimizeButtonVisibility = Visibility.Collapsed;
 
         // Init Events
-        this.Closing += OnWindowClosing;
         ViewModel.Initialize(() => ActionWrappers.ExecuteWithApplicationDispatcher(this.Close));
-    }
-
-    private void OnWindowClosing(object sender, CancelEventArgs e)
-    {
-        // Re-Set Button Visibility
-        base.ViewModel.CloseButtonVisibility = OriginalCloseVisibility;
-        base.ViewModel.MinimizeButtonVisibility = OriginalMinimizeVisibility;
-        base.ViewModel.MaximizeButtonVisibility = OriginalMaximizeVisibility;
     }
 }
