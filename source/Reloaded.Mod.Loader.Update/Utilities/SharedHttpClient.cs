3b05d52db01690403246b092d9989bc4fb8267a7
Sewer 56 <seweratwork@gmail.com>
2022-08-09 22:10:00 +0100

    Added: Lazy Initialization to SharedHttpClient

--- source/Reloaded.Mod.Loader.Update/Utilities/SharedHttpClient.cs
+++ source/Reloaded.Mod.Loader.Update/Utilities/SharedHttpClient.cs
@@ -9,24 +9,60 @@ namespace Reloaded.Mod.Loader.Update.Utilities;
 /// </summary>
 public static class SharedHttpClient
 {
+    private static HttpClient? _cachedAndCompressed;
+    private static HttpClient? _cached;
+    private static HttpClient? _uncachedAndCompressed;
+
     /// <summary>
     /// Client with support for caching and decompression.
     /// </summary>
-    public static readonly HttpClient CachedAndCompressed = AkavacheWebCacheStore.Instance.CreateClient(new HttpClientHandler()
+    public static HttpClient CachedAndCompressed
     {
-        AutomaticDecompression = DecompressionMethods.All
-    });
+        get
+        {
+            if (_cachedAndCompressed != null)
+                return _cachedAndCompressed;
+
+            _cachedAndCompressed = AkavacheWebCacheStore.Instance.CreateClient(new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.All
+            });
+
+            return _cachedAndCompressed;
+        }
+    }
 
     /// <summary>
     /// Client with support for caching.
     /// </summary>
-    public static readonly HttpClient Cached = AkavacheWebCacheStore.Instance.CreateClient();
+    public static HttpClient Cached
+    {
+        get
+        {
+            if (_cached != null)
+                return _cached;
+
+            _cached = AkavacheWebCacheStore.Instance.CreateClient();
+            return _cached;
+        }
+    }
 
     /// <summary>
     /// Client with support for only decompression.
     /// </summary>
-    public static readonly HttpClient UncachedAndCompressed = new(new HttpClientHandler()
+    public static HttpClient UncachedAndCompressed
     {
-        AutomaticDecompression = DecompressionMethods.All
-    });
+        get
+        {
+            if (_uncachedAndCompressed != null)
+                return _uncachedAndCompressed;
+
+            _uncachedAndCompressed = new(new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.All
+            });
+
+            return _uncachedAndCompressed;
+        }
+    }
 }
