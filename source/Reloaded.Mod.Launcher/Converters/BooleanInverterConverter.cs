2a6b55c4d0a9b4d9bf0f95104f2224bd335e09dc
Sewer 56 <seweratwork@gmail.com>
2022-10-09 09:00:23 +0100

    Changed: Search Settings are now Stored in Popup

---
+++ source/Reloaded.Mod.Launcher/Converters/BooleanInverterConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Reloaded.Mod.Launcher.Converters;
+
+public class BooleanInverterConverter : IValueConverter
+{
+    public static BooleanInverterConverter Instance = new BooleanInverterConverter();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+            return !b;
+
+        return value;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Convert(value, targetType, parameter, culture);
+    }
+}
