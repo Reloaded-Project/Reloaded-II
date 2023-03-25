6af45668ef02642395642a861d323f9dcf09fee1
James Rowe <jroweboy@gmail.com>
2023-03-25 16:13:37 -0400

    File and Folder picker

--- source/Reloaded.Mod.Interfaces/Structs/ControlAttribute.cs
+++ source/Reloaded.Mod.Interfaces/Structs/ControlAttribute.cs
@@ -43,3 +43,81 @@ public class SliderControlParamsAttribute : Attribute, ICustomControlAttribute
         TickPlacement = tickPlacement;
     }
 }
+
+[System.AttributeUsage(System.AttributeTargets.Property)]
+public class FilePickerParamsAttribute : Attribute, ICustomControlAttribute
+{
+    public string InitialDirectory { get; }
+    public Environment.SpecialFolder InitialFolderPath { get; }
+    public string ChooseFileButtonLabel { get; }
+    public string Title { get; }
+    public string Filter { get; }
+    public int FilterIndex { get; }
+    public bool Multiselect { get; }
+    public bool SupportMultiDottedExtensions { get; }
+    public bool ShowHiddenFiles { get; }
+    public bool ShowPreview { get; }
+    public bool RestoreDirectory { get; }
+    public bool AddToRecent { get; }
+
+    public FilePickerParamsAttribute(
+        string initialDirectory = null,
+        Environment.SpecialFolder initialFolderPath = Environment.SpecialFolder.Personal,
+        string chooseFileButtonLabel = "Choose File",
+        string title = "",
+        string filter = "All files (*.*)|*.*",
+        int filterIndex = 0,
+        bool multiselect = false,
+        bool supportMultiDottedExtensions = false,
+        bool showHiddenFiles = false,
+        bool showPreview = false,
+        bool restoreDirectory = false,
+        bool addToRecent = false
+    ) {
+        InitialDirectory = initialDirectory == null ? initialDirectory : Environment.GetFolderPath(initialFolderPath);
+        InitialFolderPath = initialFolderPath;
+        ChooseFileButtonLabel = chooseFileButtonLabel;
+        Title = title;
+        Filter = filter;
+        FilterIndex = filterIndex;
+        Multiselect = multiselect;
+        SupportMultiDottedExtensions = supportMultiDottedExtensions;
+        ShowHiddenFiles = showHiddenFiles;
+        ShowPreview = showPreview;
+        RestoreDirectory = restoreDirectory;
+        AddToRecent = addToRecent;
+    }
+}
+
+[System.AttributeUsage(System.AttributeTargets.Property)]
+public class FolderPickerParamsAttribute : Attribute, ICustomControlAttribute
+{
+    public string InitialDirectory { get; }
+    public Environment.SpecialFolder InitialFolderPath { get; }
+    public string ChooseFolderButtonLabel { get; }
+    public string Title { get; }
+    public string OkButtonLabel { get; }
+    public string FileNameLabel { get; }
+    public bool Multiselect { get; }
+    public bool ForceFileSystem { get; }
+
+    public FolderPickerParamsAttribute(
+        string initialDirectory = null,
+        Environment.SpecialFolder initialFolderPath = Environment.SpecialFolder.Personal,
+        string chooseFolderButtonLabel = "Choose Folder",
+        string title = "",
+        string okButtonLabel = "Ok",
+        string fileNameLabel = "",
+        bool multiSelect = false,
+        bool forceFileSystem = false
+    ) {
+        InitialDirectory = initialDirectory == null ? initialDirectory : Environment.GetFolderPath(InitialFolderPath);
+        InitialFolderPath = initialFolderPath;
+        ChooseFolderButtonLabel = chooseFolderButtonLabel;
+        Title = title;
+        OkButtonLabel = okButtonLabel;
+        FileNameLabel = fileNameLabel;
+        Multiselect = multiSelect;
+        ForceFileSystem = forceFileSystem;
+    }
+}
--- source/Testing/Mods/TestModControlParams/Config.cs
+++ source/Testing/Mods/TestModControlParams/Config.cs
@@ -15,8 +15,26 @@ namespace TestModControlParams.Configuration
         [DisplayName("Double Slider")]
         [Description("This is a double that uses a slider control without any frills.")]
         [DefaultValue(0.5)]
-        [SliderControlParams(minimum:0.0, maximum:1.0)]
+        [SliderControlParams(minimum: 0.0, maximum: 1.0)]
         public double DoubleSlider { get; set; } = 0.5;
+
+        [DisplayName("File Picker")]
+        [Description("This is a sample file picker.")]
+        [DefaultValue("")]
+        [FilePickerParams(title:"Choose a File to load from")]
+        public string File { get; set; } = "";
+
+        [DisplayName("Folder Picker")]
+        [Description("Opens a file picker but locked to only allow folder selections.")]
+        [DefaultValue("")]
+        [FolderPickerParams(
+            initialFolderPath: Environment.SpecialFolder.Desktop,
+            title: "Custom Folder Select",
+            okButtonLabel: "Choose Folder",
+            fileNameLabel: "ModFolder",
+            multiSelect: true,
+            forceFileSystem: true)]
+        public string Folder { get; set; } = "";
     }
 
     /// <summary>
