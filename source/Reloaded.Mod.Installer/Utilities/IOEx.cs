b6e2e938580d0f936c986a28b39a6276228e41e7
Sewer56 <admin@sewer56.dev>
2024-09-19 06:19:46 +0100

    Setup: Don't start the process as administrator.

--- source/Reloaded.Mod.Installer/Utilities/IOEx.cs
+++ source/Reloaded.Mod.Installer/Utilities/IOEx.cs
@@ -1,98 +1,8 @@
-using Path = System.IO.Path;
-
 namespace Reloaded.Mod.Installer.Utilities
 {
     // ReSharper disable once InconsistentNaming
     public static class IOEx
     {
-        /// <summary>
-        /// Moves a directory from a given source path to a target path, overwriting all files.
-        /// </summary>
-        /// <param name="source">The source path.</param>
-        /// <param name="target">The target path.</param>
-        public static void MoveDirectory(string source, string target)
-        {
-            MoveDirectory(source, target, (x, y) =>
-            {
-                File.Copy(x, y, true);
-                File.Delete(x);
-            });
-        }
-
-        /// <summary>
-        /// Copies a directory from a given source path to a target path, overwriting all files.
-        /// </summary>
-        /// <param name="source">The source path.</param>
-        /// <param name="target">The target path.</param>
-        public static void CopyDirectory(string source, string target)
-        {
-            MoveDirectory(source, target, (x, y) => File.Copy(x, y, true));
-        }
-
-        private static void MoveDirectory(string source, string target, Action<string, string> moveDirectoryAction)
-        {
-            Directory.CreateDirectory(target);
-
-            // Get all files in source directory.
-            var sourceFilePaths = Directory.EnumerateFiles(source);
-
-            // Move them.
-            foreach (var sourceFilePath in sourceFilePaths)
-            {
-                // Get destination file path
-                var destFileName = Path.GetFileName(sourceFilePath);
-                var destFilePath = Path.Combine(target, destFileName);
-
-                while (File.Exists(destFilePath) && !CheckFileAccess(destFilePath, FileMode.Open, FileAccess.Write))
-                    Thread.Sleep(100);
-
-                if (File.Exists(destFilePath))
-                    File.Delete(destFilePath);
-
-                moveDirectoryAction(sourceFilePath, destFilePath);
-            }
-
-            // Get all subdirectories in source directory.
-            var sourceSubDirPaths = Directory.EnumerateDirectories(source);
-
-            // Recursively move them.
-            foreach (var sourceSubDirPath in sourceSubDirPaths)
-            {
-                var destSubDirName = Path.GetFileName(sourceSubDirPath);
-                var destSubDirPath = Path.Combine(target, destSubDirName);
-                MoveDirectory(sourceSubDirPath, destSubDirPath, moveDirectoryAction);
-            }
-        }
-
-        /// <summary>
-        /// Tries to open a stream for a specified file.
-        /// Returns null if it fails due to file lock.
-        /// </summary>
-        public static FileStream? TryOpenOrCreateFileStream(string filePath, FileMode mode = FileMode.OpenOrCreate, FileAccess access = FileAccess.ReadWrite)
-        {
-            try
-            {
-                return File.Open(filePath, mode, access);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                return null;
-            }
-            catch (IOException)
-            {
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// Checks whether a file with a specific path can be opened.
-        /// </summary>
-        public static bool CheckFileAccess(string filePath, FileMode mode = FileMode.Open, FileAccess access = FileAccess.ReadWrite)
-        {
-            using var stream = TryOpenOrCreateFileStream(filePath, mode, access);
-            return stream != null;
-        }
-
         /// <summary>
         /// Tries to delete a directory, if possible.
         /// </summary>
@@ -100,24 +10,6 @@ namespace Reloaded.Mod.Installer.Utilities
         {
             try { Directory.Delete(path, recursive); }
             catch (Exception) { /* Ignored */ }
-        }
-
-        /// <summary>
-        /// Tries to delete a directory, if possible.
-        /// </summary>
-        public static void TryDeleteFile(string path)
-        {
-            try { File.Delete(path); }
-            catch (Exception) { /* Ignored */ }
-        }
-
-        /// <summary>
-        /// Tries to empty a directory, if possible.
-        /// </summary>
-        public static void TryEmptyDirectory(string path)
-        {
-            TryDeleteDirectory(path, true);
-            Directory.CreateDirectory(path);
         }
     }
 }
--- source/Reloaded.Mod.Installer/Utilities/Native.cs
+++
@@ -1,7 +0,0 @@
-namespace Reloaded.Mod.Installer.Utilities;
-
-public static class Native
-{
-    [DllImport("Shlwapi.dll", EntryPoint = "PathIsDirectoryEmptyW", CharSet = CharSet.Unicode)]
-    public static extern bool IsDirectoryEmpty(string directory);
-}
