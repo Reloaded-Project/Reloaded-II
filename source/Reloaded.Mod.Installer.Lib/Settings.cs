99c02b8d9fdd8cbd358bf905a42d8c5c8327c05f
Sewer56 <sewer56lol@googlemail.com>
2024-09-20 20:35:57 +0100

    Fixed: Now correctly installs to home directory on Proton

--- source/Reloaded.Mod.Installer.Lib/Settings.cs
+++ source/Reloaded.Mod.Installer.Lib/Settings.cs
@@ -3,7 +3,7 @@
 /// <summary>
 ///     Settings for the installer.
 /// </summary>
-public struct Settings
+public class Settings
 {
     public string InstallLocation { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "Reloaded-II");
     public bool IsManuallyOverwrittenLocation { get; set; }
