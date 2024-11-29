8e015957de04099bafa40a4d69554edbb0702d6e
Sewer56 <sewer56lol@googlemail.com>
2024-11-29 13:41:56 +0000

    Added: Sanitize file name for Protontricks shortcuts

--- source/Reloaded.Mod.Installer.Lib/MainWindowViewModel.cs
+++ source/Reloaded.Mod.Installer.Lib/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 using System.IO.Compression;
+using System.Linq;
 using System.Net.Http;
 using Windows.Win32.Security;
 using Windows.Win32.System.Threading;
@@ -161,6 +162,7 @@ StartupWMClass=reloaded-ii.exe
         desktopFile = desktopFile.Replace("{RELOADEDFOLDER}", Path.GetDirectoryName(nativeExecutablePath)!.Replace('\\', '/'));
         desktopFile = desktopFile.Replace("{NATIVEPATH}", nativeExecutablePath);
         shortcutPath = shortcutPath.Replace(".lnk", ".desktop");
+        shortcutPath = SanitizeFileName(shortcutPath);
         
         File.WriteAllText(shortcutPath, desktopFile);
 
@@ -374,4 +376,24 @@ StartupWMClass=reloaded-ii.exe
                 "Error in Configuring WINE", 0x0);
         }
     }
+    
+    /// <summary>
+    /// Sanitizes a file or path name to not contain invalid chars.
+    /// </summary>
+    /// <returns>Sanitized file name.</returns>
+    private static string SanitizeFileName(string fileName)
+    {
+        var invalidChars = GetInvalidFileNameChars();
+        return new string(fileName.Where(x => !invalidChars.Contains(x)).ToArray());
+    }
+    
+    // Copied from Windows version, Unix version is a subset of this.
+    private static char[] GetInvalidFileNameChars() =>
+    [
+        '\"', '<', '>', '|', '\0',
+        (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+        (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+        (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+        (char)31, ':', '*', '?', '\\', '/'
+    ];
 }
