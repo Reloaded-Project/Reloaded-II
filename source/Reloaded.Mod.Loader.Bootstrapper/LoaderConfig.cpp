f7839a3ff534c85ac43cb92afda9701df1e4b7bc
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2025-04-08 01:09:28 +0530

    Fixed https://github.com/Reloaded-Project/Reloaded-II/issues/492

--- source/Reloaded.Mod.Loader.Bootstrapper/LoaderConfig.cpp
+++ source/Reloaded.Mod.Loader.Bootstrapper/LoaderConfig.cpp
@@ -6,9 +6,13 @@
 #include <locale>
 #include <codecvt>
 #include "ReloadedPaths.h"
+#include <Windows.h>
+#include <string>
+#include <fileapi.h>
 
 LoaderConfig::LoaderConfig()
 {
+
 	// Get path to AppData
 	char_t buffer[32767]; // Max Windows10+ path.
 	BOOL result = SHGetSpecialFolderPath(nullptr, buffer, CSIDL_APPDATA, false);
@@ -28,6 +32,51 @@ LoaderConfig::LoaderConfig()
 	config = json::parse(configFile);
 }
 
+string_t GetCurrentModulePath()
+{
+	wchar_t rawPath[MAX_PATH];
+	HMODULE hModule = GetModuleHandle(nullptr);
+
+	if (hModule == nullptr)
+		return L"Unable to get address";
+
+	DWORD result = GetModuleFileNameW(hModule, rawPath, MAX_PATH);
+	if (result == 0 || result == MAX_PATH)
+		return L"Unable to get address";
+
+	// Open the file to get a handle
+	HANDLE fileHandle = CreateFileW(
+		rawPath,
+		0,
+		FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+		nullptr,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		nullptr
+	);
+
+	if (fileHandle == INVALID_HANDLE_VALUE)
+		return string_t(rawPath);
+
+	wchar_t finalPath[MAX_PATH];
+	DWORD finalResult = GetFinalPathNameByHandleW(fileHandle, finalPath, MAX_PATH, FILE_NAME_NORMALIZED);
+	CloseHandle(fileHandle);
+
+	if (finalResult == 0 || finalResult >= MAX_PATH)
+		return string_t(rawPath);
+
+	string_t resultPath = finalPath;
+	const string_t prefix = L"\\\\?\\";
+	if (resultPath.rfind(prefix, 0) == 0)
+		resultPath = resultPath.substr(prefix.length());
+
+	size_t lastSlash = resultPath.find_last_of(L"\\/");
+	if (lastSlash != string_t::npos)
+		resultPath = resultPath.substr(0, lastSlash);
+
+	return resultPath;
+}
+
 string_t LoaderConfig::get_loader_path()
 {
 	#if _WIN64
@@ -45,8 +94,14 @@ string_t LoaderConfig::get_loader_path()
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 	const string_t loaderPath = converter.from_bytes(stringLoaderPath);
 
-	if (!Utilities::file_exists(loaderPath))
-		throw std::exception("Reloaded Mod Loader DLL has not been found.");
+	if (!Utilities::file_exists(loaderPath)) {
+		std::string dllFolderPath = converter.to_bytes(GetCurrentModulePath());
+		std::string errorMessage = "Reloaded-II Loader DLL has not been found.\nTo fix this, start the Reloaded launcher.";
+		if (dllFolderPath != "Unable to get address") {
+			errorMessage = "Reloaded-II Loader DLL has not been found.\nTo fix this, start the Reloaded launcher.\n\nIf you intended to uninstall Reloaded, delete:\n-" + dllFolderPath + "\\DSOUND.dll\n-" + dllFolderPath + "\\Reloaded.Mod.Loader.Bootstrapper.asi";
+		}
+		throw std::exception(errorMessage.c_str());
+	}		
 
 	return loaderPath;
 }
