c42c5143658a46a5522ab27d7732043677ad0ce6
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2025-04-07 22:10:07 +0530

    Made exception messageboxes more user friendly and moved stacktrace logging to log files

--- source/Reloaded.Mod.Launcher.Lib/Static/Errors.cs
+++ source/Reloaded.Mod.Launcher.Lib/Static/Errors.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using static System.Environment;
 
 namespace Reloaded.Mod.Launcher.Lib.Static;
 
@@ -22,11 +23,38 @@ public static class Errors
                 // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
                 if (Actions.DisplayMessagebox != null)
                 {
-                    Actions.DisplayMessagebox.Invoke(Resources.ErrorUnknown.Get(), $"{message}{ex.Message}\n{ex.StackTrace}");
+                    var logPath = $"{Path.Combine(GetFolderPath(SpecialFolder.ApplicationData), "Reloaded-Mod-Loader-II", "ApplicationLogs", DateTime.UtcNow.ToString("yyyy-MM-dd HH.mm.ss"))}.txt";
+                    bool result = Actions.DisplayMessagebox.Invoke(Resources.ErrorUnknown.Get(), $"{message}{ex.Message}\nDo you wish to view the stacktrace for more information?", new Actions.DisplayMessageBoxParams
+                    {
+                        Type = Actions.MessageBoxType.OkCancel
+                    });
+                    if (result)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(logPath)!);
+                        File.WriteAllText(logPath, $"Exception:\n{ex.Message}\nStacktrace:\n{ex.StackTrace}");
+                        ProcessStartInfo logFile = new()
+                        {
+                            FileName = logPath,
+                            UseShellExecute = true
+                        };
+                        Process.Start(logFile);
+                    }
                 }
                 else
                 {
-                    MessageBox.Show($"{message}{ex.Message}\n{ex.StackTrace}", Resources.ErrorUnknown.Get());
+                    var logPath = $"{Path.Combine(GetFolderPath(SpecialFolder.ApplicationData), "Reloaded-Mod-Loader-II", "ApplicationLogs", DateTime.UtcNow.ToString("yyyy-MM-dd HH.mm.ss"))}.txt";
+                    var result = MessageBox.Show($"{message}{ex.Message}\nDo you wish to view the stacktrace for more information?", Resources.ErrorUnknown.Get(), MessageBoxButton.YesNo, MessageBoxImage.Error);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(logPath)!);
+                        File.WriteAllText(logPath, $"Exception:\n{ex.Message}\nStacktrace:\n{ex.StackTrace}");
+                        ProcessStartInfo logFile = new()
+                        {
+                            FileName = logPath,
+                            UseShellExecute = true
+                        };
+                        Process.Start(logFile);
+                    }
                 }
             }
         }
